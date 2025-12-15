af660126dc41242d6923f9304c30c17e0a4ef15a
Shane Barron <shane@Mac.hsd1.fl.comcast.net>
2025-12-15 18:58:10 -0500

    Fix PHP syntax - use PascalCase for all class names

--- src/CopyButtonServiceProvider.php
+++ src/CopyButtonServiceProvider.php
@@ -3,14 +3,36 @@
 namespace MrShaneBarron\CopyButton;
 
 use Illuminate\Support\ServiceProvider;
+use MrShaneBarron\CopyButton\Livewire\CopyButton;
+use MrShaneBarron\CopyButton\View\Components\CopyButton as BladeCopyButton;
 
 class CopyButtonServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/sb-copy-button.php', 'sb-copy-button');
+    }
+
     public function boot(): void
     {
-        if (class_exists(\Livewire\Livewire::class)) {
-            \Livewire\Livewire::component('sb-copy-button', Livewire\CopyButton::class);
-        }
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sb-copy-button');
+
+        if (class_exists(\Livewire\Livewire::class)) {
+            \Livewire\Livewire::component('sb-copy-button', CopyButton::class);
+        }
+
+        $this->loadViewComponentsAs('ld', [
+            BladeCopyButton::class,
+        ]);
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/sb-copy-button.php' => config_path('sb-copy-button.php'),
+            ], 'sb-copy-button-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/sb-copy-button'),
+            ], 'sb-copy-button-views');
+        }
     }
 }
